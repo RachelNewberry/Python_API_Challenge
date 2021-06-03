313f0ff9a35da4d4e368fcd960662253dcc8b7af
RachelNewberry <rachellnewbery@yahoo.com>
2021-06-03 12:23:01 -0400

    Saving progress, imports, cities generated, and API calls...complete.

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6410ecab8af797ed15e7b83e9e2b53c8"
 
 # Google API Key
-g_key = "YOUR KEY HERE"
+g_key = "AIzaSyCFpEiuQ2k-GGprGZsLOtV240IuUQerDMs"
